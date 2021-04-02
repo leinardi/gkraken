599cac8ea7739c688c20bfbd2957fcc0bcf5d204
Guy Boldon <gb@guyboldon.com>
2021-04-02 14:25:46 +0200

    add fix for line and text chart colors in dark/light mode

--- gkraken/util/view.py
+++ gkraken/util/view.py
@@ -50,10 +50,19 @@ def hide_on_delete(widget: Gtk.Widget, *_: Any) -> Any:
     return widget.hide_on_delete()
 
 
+def rgba_to_hex(color: Gdk.RGBA) -> str:
+    """Return hexadecimal string for :class:`Gdk.RGBA` `color`."""
+    return "#{0:02x}{1:02x}{2:02x}{3:02x}".format(int(color.red * 255),
+                                                  int(color.green * 255),
+                                                  int(color.blue * 255),
+                                                  int(color.alpha * 255))
+
+
 def init_plot_chart(scrolled_window: Gtk.ScrolledWindow,
                     figure: Figure,
                     canvas: FigureCanvas,
                     axis: Axes) -> Any:
+    # 3.4.1 display workaround:
     axis.patch.set_visible(False)
     temp_window = Gtk.Window()
     style = temp_window.get_style_context()
@@ -61,11 +70,21 @@ def init_plot_chart(scrolled_window: Gtk.ScrolledWindow,
     cc = ColorConverter()
     cc.to_rgba(bg_colour)
     figure.patch.set_facecolor(bg_colour)
+
+    # correct text and line colors
+    temp_label = Gtk.Label()
+    scrolled_window.add(temp_label)
+    text_color = rgba_to_hex(temp_label.get_style_context().get_color(Gtk.StateType.NORMAL))
+    text_color_alpha = text_color[:-2] + '80'
+    scrolled_window.remove(temp_label)
+
     axis.grid(True, linestyle=':')
     axis.margins(x=0, y=0.05)
-
-    axis.set_xlabel('Liquid temperature [°C]')
-    axis.set_ylabel('Duty [%]')
+    axis.set_xlabel('Liquid temperature [°C]', color=text_color)
+    axis.set_ylabel('Duty [%]', color=text_color)
+    axis.tick_params(colors=text_color, grid_color=text_color_alpha)
+    for spine in axis.spines.values():
+        spine.set_edgecolor(text_color_alpha)
     figure.subplots_adjust(top=1)
     canvas.set_size_request(400, 300)
     scrolled_window.add_with_viewport(canvas)
